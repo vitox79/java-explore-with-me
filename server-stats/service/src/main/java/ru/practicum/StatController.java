3b11ec0ea2b80c2d82409df6d570511ea4e85d1a
vitox79 <viktor.monarev@gmail.com>
2023-08-11 15:37:02 +0600

    refactor2

--- server-stats/service/src/main/java/ru/practicum/StatController.java
+++ server-stats/service/src/main/java/ru/practicum/StatController.java
@@ -16,18 +16,18 @@ public class StatController {
     private final StatService service;
 
     @PostMapping("/hit")
-    public HitDto createStat(@RequestBody @Valid HitDto dto) {
-        log.info("Обновление статистики: сохранение {}", dto);
+    public HitDto addStatistics(@RequestBody @Valid HitDto dto) {
+        log.info("Refresh data: saving {}", dto);
         return service.create(dto);
     }
 
     @GetMapping("/stats")
-    public List<StatsDto> getStats(@RequestParam("start") String startStr,
-                                   @RequestParam("end") String endStr,
-                                   @RequestParam(required = false) List<String> uris,
-                                   @RequestParam(defaultValue = "false", required = false) Boolean unique) {
-        log.info("Получение статистики с параметрами: start {}, end {}, uris {}, unique {}",
-                startStr, endStr, uris, unique);
+    public List<StatsDto> getStatistics(@RequestParam("start") String startStr,
+                                        @RequestParam("end") String endStr,
+                                        @RequestParam(required = false) List<String> uris,
+                                        @RequestParam(defaultValue = "false", required = false) Boolean unique) {
+        log.info("Get statistics with: start {}, end {}, uris {}, unique {}",
+            startStr, endStr, uris, unique);
         return service.getStatus(startStr, endStr, uris, unique);
     }
 }
